ec224b687655cdd90cffb7fca77ec5a14a209742
Meet Palod <meetpalod18@gmail.com>
2020-08-05 17:02:20 +0530

    For sum query and  MRQ

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,3 +1,59 @@
+// FOR SUM QUERY
+#include <bits/stdc++.h> 
+using namespace std; 
+
+const int N = 100000; 
+int n; // array size 
+int tree[2 * N]; 
+
+void build( int arr[]) 
+{ 
+	for (int i=0; i<n; i++)	 
+		tree[n+i] = arr[i]; 	
+	for (int i = n - 1; i > 0; --i)	 
+		tree[i] = tree[i<<1] + tree[i<<1 | 1];	 
+} 
+
+void updateTreeNode(int p, int value) 
+{ 
+	tree[p+n] = value; 
+	p = p+n; 
+	for (int i=p; i > 1; i >>= 1) 
+		tree[i>>1] = tree[i] + tree[i^1]; 
+} 
+
+int query(int l, int r) 
+{ 
+	int res = 0; 
+	
+	// loop to find the sum in the range 
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1) 
+	{ 
+		if (l&1) 
+			res += tree[l++]; 
+	
+		if (r&1) 
+			res += tree[--r]; 
+	} 
+	
+	return res; 
+} 
+
+int main() 
+{ 
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; 
+	n = sizeof(a)/sizeof(a[0]); 
+	build(a); 
+	cout << query(1, 3)<<endl; 
+	updateTreeNode(2, 1); 
+	cout << query(1, 3)<<endl; 
+
+	return 0; 
+} 
+
+
+// FOR MIN RANGE QUERY
+
 #include "bits/stdc++.h"
 #define MODULO 1000000007
 #define endl '\n'
